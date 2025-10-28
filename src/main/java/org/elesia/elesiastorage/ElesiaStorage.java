0fe3475870096bfee5e5c71f86aea48ce3057f7d
Kilian <coubard.k18@gmail.com>
2025-10-28 12:11:56 +0100

    initial commit

---
+++ src/main/java/org/elesia/elesiastorage/ElesiaStorage.java
@@ -0,0 +1,17 @@
+package org.elesia.elesiastorage;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ElesiaStorage extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
